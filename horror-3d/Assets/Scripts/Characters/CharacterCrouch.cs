3c3bfddac32049e9de49d2f187d3843b60276193
Pawee Chinagarn <pawee.chinagarn@gmail.com>
2023-08-24 16:05:37 +0700

    Implement crouching

---
+++ horror-3d/Assets/Scripts/Characters/CharacterCrouch.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+namespace Horror3D
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterCrouch : MonoBehaviour
+    {
+        private enum CrouchingState
+        {
+            Stand,
+            Crouch,
+            PrepareToStand
+        }
+
+        [SerializeField] private CharacterController characterController;
+        [SerializeField] private float crouchingHeight;
+        [SerializeField] private float standingHeight;
+        [SerializeField] private LayerMask obstacleLayerMask;
+
+        private CrouchingState state;
+        private readonly RaycastHit[] checkHeadResult = new RaycastHit[1];
+
+        private void Awake()
+        {
+            characterController = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            if (state != CrouchingState.PrepareToStand)
+                return;
+
+            var hits = Physics.RaycastNonAlloc(transform.position, Vector3.up, checkHeadResult, standingHeight, obstacleLayerMask);
+            if (hits == 0)
+            {
+                Stand();
+            }
+        }
+
+
+        public void OnCrouch(InputAction.CallbackContext context)
+        {
+            if (context.interaction is not HoldInteraction)
+                return;
+
+            Debug.Log($"hold c {context.phase}");
+            switch (context.phase)
+            {
+                case InputActionPhase.Performed:
+                    Crouch();
+                    break;
+                case InputActionPhase.Canceled:
+                    state = CrouchingState.PrepareToStand;
+                    break;
+            }
+        }
+
+        private void Crouch()
+        {
+            state = CrouchingState.Crouch;
+            characterController.height = crouchingHeight;
+            characterController.center = new Vector3(characterController.center.x, crouchingHeight * 0.5f, characterController.center.z);
+        }
+
+        private void Stand()
+        {
+            characterController.height = standingHeight;
+            characterController.center = new Vector3(characterController.center.x, standingHeight * 0.5f, characterController.center.z);
+            state = CrouchingState.Stand;
+        }
+    }
+}
