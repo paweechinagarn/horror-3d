f0b160d03fa216abe3ea3f51c33dd680bc800d77
Pawee Chinagarn <pawee.chinagarn@gmail.com>
2023-08-24 14:41:35 +0700

    Running and smooth rotation

--- horror-3d/Assets/Scripts/PlayerMovement3D.cs
+++ horror-3d/Assets/Scripts/PlayerMovement3D.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 namespace Horror3D
 {
@@ -7,9 +8,12 @@ namespace Horror3D
     public class PlayerMovement3D : MonoBehaviour
     {
         [SerializeField] private CharacterController characterController;
-        [SerializeField] private float moveSpeed = 10f;
+        [SerializeField] private float walkSpeed = 5f;
+        [SerializeField] private float runSpeed = 10f;
+        [SerializeField] private float turnSpeed = 10f;
 
         private Vector2 moveInput;
+        private bool isRunning;
 
         private void Awake()
         {
@@ -19,14 +23,43 @@ namespace Horror3D
         private void FixedUpdate()
         {
             var cameraTransform = Camera.main.transform;
+
+            // Calculate move direction
             var direction = cameraTransform.forward * moveInput.y + cameraTransform.right * moveInput.x;
             direction.y = 0f;
-            characterController.Move(moveSpeed * Time.fixedDeltaTime * direction);
+
+            // Move with walk speed or run speed
+            characterController.Move((isRunning ? runSpeed : walkSpeed) * Time.fixedDeltaTime * direction);
+
+            // Looking towards move direction
+            if (direction != Vector3.zero)
+            {
+                var rotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.fixedDeltaTime);
+            }
         }
 
         public void OnMove(InputAction.CallbackContext context)
         {
             moveInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnRun(InputAction.CallbackContext context)
+        {
+            if (context.interaction is not HoldInteraction)
+                return;
+
+            Debug.Log($"hold shift {context.phase}");
+            switch (context.phase)
+            {
+                case InputActionPhase.Performed:
+                    isRunning = true;
+                    break;
+                case InputActionPhase.Canceled:
+                    isRunning = false;
+                    break;
+            }
+            Debug.Log($"isRunning = {isRunning}");
         }
     }
 }
