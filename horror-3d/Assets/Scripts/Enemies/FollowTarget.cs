a462020f00df8cf27a19a4600b60406624cc39cc
Pawee Chinagarn <pawee.chinagarn@gmail.com>
2023-08-25 15:44:56 +0700

    Implement basic enemy with following target behavior

---
+++ horror-3d/Assets/Scripts/Enemies/FollowTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Horror3D
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent agent;
+
+        public Transform Eye;
+        public Transform Target;
+
+        private readonly RaycastHit[] hitResults = new RaycastHit[10];
+
+        private void Update()
+        {
+            var direction = Target.position - Eye.position;
+            direction.y = 0f;
+
+            Debug.DrawRay(Eye.position, direction);
+
+            var hits = Physics.RaycastNonAlloc(Eye.position, direction, hitResults);
+            if (hits == 0)
+                return;
+
+            var sortedHitResults = hitResults.Where(x => x.transform != null).OrderBy(x => x.distance);
+
+            var firstHitResult = sortedHitResults.First();
+            if (firstHitResult.transform == Target)
+            {
+                agent.SetDestination(Target.position);
+            }
+        }
+    }
+}
