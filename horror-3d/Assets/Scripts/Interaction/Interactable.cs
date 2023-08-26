391c2ba4e1038d4ac16c0c1fc6f2a7c283f6d6a8
Pawee Chinagarn <pawee.chinagarn@gmail.com>
2023-08-26 23:04:03 +0700

    Add more scenario

---
+++ horror-3d/Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Horror3D
+{
+    public class Interactable : MonoBehaviour, IInteractable
+    {
+        public UnityEvent OnInteracted;
+
+        public InteractionAmountMode AmountMode => InteractionAmountMode.Once;
+
+        public void Interact()
+        {
+            OnInteracted?.Invoke();
+        }
+    }
+}
