e6f3a14635adaf5a0cb51b70b9a969eca3899767
Pawee Chinagarn <pawee.chinagarn@gmail.com>
2023-08-26 16:58:02 +0700

    Add simple footstep sfx

---
+++ horror-3d/Assets/Scripts/Enemies/PlaySoundOnMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Horror3D
+{
+    public class PlaySoundOnMove : MonoBehaviour
+    {
+        [SerializeField] private AudioSource source;
+        [SerializeField] private NavMeshAgent agent;
+
+        private void Update()
+        {
+            if (agent.velocity != Vector3.zero && !source.isPlaying)
+            {
+                source.Play();
+            }
+            else if (agent.velocity == Vector3.zero && source.isPlaying)
+            {
+                source.Stop();
+            }
+        }
+    }
+}
