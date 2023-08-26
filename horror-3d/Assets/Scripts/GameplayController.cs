6979ce499542459ba766cb972e2d256649e47227
Pawee Chinagarn <pawee.chinagarn@gmail.com>
2023-08-26 17:25:07 +0700

    Add gameplay controller simply to delay input for now

---
+++ horror-3d/Assets/Scripts/GameplayController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Horror3D
+{
+    public class GameplayController : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(1f);
+            playerInput.enabled = true;
+            Debug.Log($"Game starts");
+        }
+    }
+}
